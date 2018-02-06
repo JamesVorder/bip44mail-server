1412339d7eaa327ab9908d669297f5444a90cd0f
JamesVorder <jamesvorder@gmail.com>
2018-02-06 16:04:59 -0600

    Added a send endpoint for sending mail. Restructured address validation a bit.

--- models/Address.js
+++ models/Address.js
@@ -4,14 +4,13 @@ var lightwallet = require("eth-lightwallet");
 function Address(signature, msg, addr){
     this.location = addr;
     this.signature = JSON.parse(signature);
-    this.msg = msg;
 };
 
-Address.prototype.isValid = function(){
+Address.prototype.isValid = function(message){
      var v = this.signature.v;
      var r = this.signature.r;
      var s = this.signature.s;
-     var result = lightwallet.signing.recoverAddress("hello world", v, util.toBuffer(r.data), util.toBuffer(s.data));
+     var result = lightwallet.signing.recoverAddress(message, v, util.toBuffer(r.data), util.toBuffer(s.data));
      console.log("Expected:\t" + this.location);
      console.log("Got:\t\t" + util.bufferToHex(result));
      return this.location == util.bufferToHex(result);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var Address = require('./models/Address.js');
 var bodyParser = require("body-parser");
+var Mailgun = require('mailgun-js');
+
 
 var app = express();
 
@@ -12,6 +14,38 @@ app.post('/validate', function(req, res) {
     var body = addr.isValid();
     console.log("Sending:\t" + JSON.stringify(body));
     return res.send(JSON.stringify(body));
+});
+
+app.post('/send', function(req, res){
+    console.log("/send endpoint called.");
+    //TODO: hide the next two values before commit
+    var api_key = "##########";
+    var domain = "bip44.email";
+    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+
+    var data = {
+        from: req.body.from,
+        to: req.body.to,
+        subject: req.body.subject,
+        html: req.body.message
+    };
+
+    mailgun.messages().send(data, function (err, body) {
+        if (err) {
+            //res.render('error', { error : err});
+            console.log("got an error: ", err);
+        }
+        else {
+            //res.render('submitted', { email : to });
+            console.log(body);
+        }
+    });
+    //TODO: This stuff...
+    //var addr = new Address(req.body.signature, req.body.address);
+    //if(addr.isValid(req.body.message)){
+    //
+    //}
 });
 
 app.listen(3000);
