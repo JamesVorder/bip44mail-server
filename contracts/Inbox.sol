3866f3c1f8fea1c710e4a8952edcb3662a193ce7
JamesVorder <jamesvorder@gmail.com>
2018-03-11 17:48:04 -0500

    Added getMessages and debugged.

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -3,13 +3,10 @@ pragma solidity ^0.4.20;
 import "https://github.com/GNSPS/solidity-bytes-utils/contracts/BytesLib.sol";
 
 contract Inbox{
-    struct Message{
-        Multihash location;
-    }
     struct Multihash{
         bytes1 version;
         bytes1 size;
-        bytes32 location;
+        bytes32 ipfs_coords;
     }
 
     function convertBytesToBytes32(bytes inBytes)
@@ -26,17 +23,31 @@ contract Inbox{
     }
 
     uint numMessages;
-    mapping(uint => Message) messages;
+    mapping(uint => Multihash) messages;
     function addMessage(bytes _multihash)
     public
     returns(bool success) {
         uint messageID = numMessages++;
-        messages[messageID] =
-        Message(
-            Multihash(
-                _multihash[1],
-                _multihash[2],
-                convertBytesToBytes32(BytesLib.slice(_multihash, 3, _multihash.length - 3))));
+        messages[messageID] =Multihash(
+            _multihash[0],
+            _multihash[1],
+            convertBytesToBytes32(BytesLib.slice(_multihash, 2, _multihash.length - 2)));
         return true;
+    }
+
+    function getMessages()
+    public
+    view
+    returns(bytes1[], bytes1[], bytes32[]){
+        bytes1[] memory versions = new bytes1[](numMessages);
+        bytes1[] memory sizes = new bytes1[](numMessages);
+        bytes32[] memory locations = new bytes32[](numMessages);
+        for (uint i = 0; i < numMessages; i++) {
+            Multihash storage mh = messages[i];
+            versions[i] = mh.version;
+            sizes[i] = mh.size;
+            locations[i] = mh.ipfs_coords;
+        }
+        return(versions, sizes, locations);
     }
 }
